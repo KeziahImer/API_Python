6ddaa1cfe8779932ba83039bbbf472950ce95e0a
Kéziah <keziah.imer@epitech.eu>
2022-07-08 17:06:37 +0200

    Edit Swagger doc

--- routes/documented_api/email_validation/__init__.py
+++ routes/documented_api/email_validation/__init__.py
@@ -6,8 +6,8 @@ namespace = Namespace('emailValidation', 'Checking if the email is good formated
 @namespace.route('')
 class emailValidation(Resource):
 
+    @namespace.param('Query String', 'http://ipadress:port/emailValidation?email=youremail')
     @namespace.response(False, 'Email is not valid')
     @namespace.response(True, 'Email is valid')
     def post(self):
         'Checking email'
-        return 'example@gmail.com?email=value'
--- routes/documented_api/health/__init__.py
+++ routes/documented_api/health/__init__.py
@@ -3,17 +3,9 @@ from flask_restx import Namespace, Resource, fields
 
 namespace = Namespace('health', 'Checking if the API is running')
 
-# health_model = namespace.model('Health', {
-#     'message': fields.String(
-#         readwrite=True,
-#         description='Is API Running ?'
-#     )
-# })
-
 @namespace.route('')
 class health(Resource):
 
-    # @namespace.marshal_list_with(health_model)
     @namespace.response(200, 'API is running')
     @namespace.response(505, 'Internal Server Error')
     def get(self):
